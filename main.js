ecf63c6e8b92e8603fc95328d97b7809711a770e
미쁨(trusty_people) <jioo0224@naver.com>
2017-03-30 21:47:25 +0900

    코드 일부 수정 2

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ server.post('*', (req, res) => {
     for(let app in apps) {
         if(apps[app].url == req.originalUrl) {
             logger.info(app+' POST Reveived');
-            let hash = 'sha1-'+crypto.createHmac('sha1', apps[app].secret).update(req.body).digest('hex');
+            let hash = 'sha1-'+crypto.createHmac('sha1', apps[app].secret).digest('hex');
             if(hash == req.headers['x-hub-signature']) {
                 exec(apps[app].command, (err, stdout, stderr) => {
                     logger.info('====== command exex ======');
