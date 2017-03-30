73c4dd4546d7ba8a967de39d642f5fb187982bd1
미쁨(trusty_people) <jioo0224@naver.com>
2017-03-30 22:47:43 +0900

    express 버림

--- main.js
+++ main.js
@@ -1,48 +1,52 @@
 process.env.NODE_ENV = ( process.env.NODE_ENV && ( process.env.NODE_ENV ).trim().toLowerCase() == 'production' ) ? 'production' : 'development';
 
-const http = require('http'), express = require('express'), crypto = require('crypto'), exec = require('child_process').exec,
+const http = require('http'), crypto = require('crypto'), exec = require('child_process').exec,
 apps = require(__dirname+'/apps.json'), config = require(__dirname+'/config.json'), logger = require('log4js').getLogger(),
-bodyParser = require('body-parser');
 
-const server = express();
-
-server.use(bodyParser.urlencoded({extended: true}));
-server.use(bodyParser.text());
-
-server.post('*', (req, res) => {
-    for(let app in apps) {
-        if(apps[app].url == req.originalUrl) {
-            logger.info(app+' POST Reveived');
-            let hash = 'sha1='+crypto.createHmac('sha1', apps[app].secret).update(JSON.stringify(req.body), 'utf8').digest('hex');
-            if(hash == req.headers['x-hub-signature']) {
-                exec(apps[app].command, (err, stdout, stderr) => {
-                    logger.info('====== command exex ======');
-                    logger.info(stdout);
-                    logger.info('====== command exec ======');
-                    if(err) {
-                        logger.error('====== command error ======');
-                        logger.error(err);
-                        logger.error('====== command error ======');
-                    }
-                    if(stderr) {
-                        logger.error('====== command stderr ======');
-                        logger.error(stderr);
-                        logger.error('====== command stderr ======');
-                    }
-                    res.send('').end();
+const server = http.createServer((req, res) => {
+    if(req.method == 'POST') {
+        for(let app in apps) {
+            if(apps[app].url == req.url) {
+                logger.info(app+' POST Reveived');
+                let body = '';
+                req.on('data', (data) => {
+                    body += data;
                 });
-            } else {
-                res.status(400);
-                logger.error('Key is not match!');
-                logger.info('Github key is '+req.headers['x-hub-signature']);
-                logger.info('and local task key is '+hash);
-                res.json({error: 'Key is not match'}).end();
+                req.on('end', () => {
+                    let hash = 'sha1='+crypto.createHmac('sha1', apps[app].secret).update(body).digest('hex');
+                    if(hash == req.headers['x-hub-signature']) {
+                        exec(apps[app].command, (err, stdout, stderr) => {
+                            logger.info('====== command exex ======');
+                            logger.info(stdout);
+                            logger.info('====== command exec ======');
+                            if(err) {
+                                logger.error('====== command error ======');
+                                logger.error(err);
+                                logger.error('====== command error ======');
+                            }
+                            if(stderr) {
+                                logger.error('====== command stderr ======');
+                                logger.error(stderr);
+                                logger.error('====== command stderr ======');
+                            }
+                            res.writeHead(200, {'Content-Type': 'text/html'});
+                            res.end('post received');
+                        });
+                    } else {
+                        res.status(400);
+                        logger.error('Key is not match!');
+                        logger.info('Github key is '+req.headers['x-hub-signature']);
+                        logger.info('and local task key is '+hash);
+                        res.writeHead(400, {'Content-Type': 'text/html'});
+                        res.end('Key is not match');
+                    }
+                });
             }
         }
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('');
     }
-});
-server.all('*', (req, res) => {
-    res.send('').end();
 });
 
 const HTTPServer = require('http').createServer(server);
