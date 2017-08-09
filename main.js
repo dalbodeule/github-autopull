61a0042c161989d0bdc9c46741a0f4465df39a96
small_sunshine <jioo0224@naver.com>
2017-08-09 21:33:06 +0900

    비정상작동 재수정

--- main.js
+++ main.js
@@ -20,10 +20,11 @@ const server = http.createServer((req, res) => {
                     let hash = 'sha1='+crypto.createHmac('sha1', apps[app].secret).update(body).digest('hex');
                     if(hash == req.headers['x-hub-signature']) {
                         logger.info("Hash validation success");
+                        body = JSON.parse(body);
                         let executeCommand = '';
                         if(typeof body.ref == 'string') {
                             let branch = body.ref.split('/')[req.ref.length - 1];
-                            logger.info('brench: '+branch);
+                            logger.info('branch: '+branch);
                             if(typeof apps[app].branch[branch] == 'string') {
                                 executeCommand = apps[app].branch[req.ref.last];
                                 logger.info(branch+" has execute commands");
@@ -35,7 +36,7 @@ const server = http.createServer((req, res) => {
                                 logger.info("not has command");
                             }
                         } else {
-                            logger.info("brench not found");
+                            logger.info("branch not found");
                             logger.debug(body.ref);
                             executeCommand = null;
                         }
